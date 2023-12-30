4cb50548f00ff316d960694cf0be6f949a6e4cbf
Stijn Vermeiren <stijn@concept10.be>
2023-12-30 15:14:47 +0100

    fix: remove duplicate `w-` class

--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -13,7 +13,7 @@ export default function Tooltip(props: TooltipProps) {
       <span
         class={`${
           props.width ? "w-" + props.width : "w-24"
-        } invisible w-24 text-center px-1 py-0 absolute z-10 bg-gray-900 dark:bg-gray-400 text-gray-400 dark:text-gray-900 rounded group-hover:visible text-white`}
+        } invisible text-center px-1 py-0 absolute z-10 bg-gray-900 dark:bg-gray-400 text-gray-400 dark:text-gray-900 rounded group-hover:visible text-white`}
       >
         {props.tooltip}
       </span>
