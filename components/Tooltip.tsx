e45d86aa395dabedc355e06ab795170a11a100ef
Stijn Vermeiren <stijn@concept10.be>
2023-12-30 14:04:54 +0100

    feat: add support for tooltip width

--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -3,13 +3,18 @@ import { children } from "solid-js";
 export type TooltipProps = {
   tooltip: string;
   children: any;
+  width?: number;
 };
 
 export default function Tooltip(props: TooltipProps) {
   return (
     <div class="relative inline-block group">
       {props.children}
-      <span class="invisible w-24 text-center px-1 py-0 absolute z-10 bg-gray-900 dark:bg-gray-400 text-gray-400 dark:text-gray-900 rounded group-hover:visible text-white">
+      <span
+        class={`${
+          props.width ? "w-" + props.width : "w-24"
+        } invisible w-24 text-center px-1 py-0 absolute z-10 bg-gray-900 dark:bg-gray-400 text-gray-400 dark:text-gray-900 rounded group-hover:visible text-white`}
+      >
         {props.tooltip}
       </span>
     </div>
