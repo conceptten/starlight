ef86e966f35cbaaf2b47836330ae207553a26243
Stijn Vermeiren <stijn@concept10.be>
2024-02-10 16:47:03 +0100

    chore: dependency updates

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+	description = "Plopsaland De Panne";
+
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+	};
+
+	outputs = { self, nixpkgs }:
+	let
+		system = "x86_64-linux";
+		pkgs = nixpkgs.legacyPackages.${system};
+	in
+	{
+		devShells.${system}.default =
+			pkgs.mkShell
+				{
+					buildInputs = [
+						pkgs.corepack_20
+						pkgs.nodejs_20
+						pkgs.nodePackages.ts-node
+					];
+			};
+	};
+}
